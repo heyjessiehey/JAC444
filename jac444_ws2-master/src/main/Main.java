47e3aec58e1c5bcfceb825933d9ad7726b000acd
Jessie Ko <36706944+heyjessiehey@users.noreply.github.com>
2019-02-23 08:59:33 -0500

    Add files via upload

---
+++ jac444_ws2-master/src/main/Main.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import shape.Circle;
+import shape.CircleException;
+import shape.Shape;
+import shape.Parallelogram;
+import shape.ParallelogramException;
+import shape.Rectangle;
+import shape.Triangle;
+import shape.TriangleException;
+import shape.Square;
+
+/**
+ * @author Gayeonko
+ *
+ */
+public class Main {
+	/**
+	 * @param args
+	 * @throws CircleException, ParallelogramException, TriangleException 
+	 */
+	public static void main(String[] args) {
+		Shape[] arrayOfShapes = new Shape[50]; // array to contain all the shapes 
+		int index = 0;
+		
+		System.out.println("------->JAC 444 Assignment 1<--------");
+		System.out.println("------->Task 1 ... <--------");
+		
+		//read a file
+		try (BufferedReader br = new BufferedReader(new FileReader("Shapes.txt"))) { 
+			String s;
+			while ((s = br.readLine()) != null) {
+				String[] tokens = s.split(",");
+					if(tokens[0].equals("Circle") && tokens.length == 2){  //compare contents and proper number of tokens
+						try{
+							double value = Double.parseDouble(tokens[1]); // convert string to double
+							arrayOfShapes[index] = new Circle(value); // create Circle object
+							index++;
+						}catch(CircleException e){
+							System.out.println(e.getMessage());
+						}
+					}else if(tokens[0].equals("Triangle") && tokens.length == 4){
+						try{
+							double value1 = Double.parseDouble(tokens[1]); 
+							double value2 = Double.parseDouble(tokens[2]);
+							double value3 = Double.parseDouble(tokens[3]);
+							arrayOfShapes[index] = new Triangle(value1, value2, value3); //create Triangle object
+							index++;
+						}catch(TriangleException e){
+							System.out.println(e.getMessage());
+						}
+					}else if(tokens[0].equals("Parallelogram") && tokens.length == 3){
+						try{
+							double value1 = Double.parseDouble(tokens[1]);
+							double value2 = Double.parseDouble(tokens[2]);
+							arrayOfShapes[index] = new Parallelogram(value1, value2); // create Parallelogram object
+							index++;
+						}catch(ParallelogramException e){
+							System.out.println(e.getMessage());
+						}
+					}else if(tokens[0].equals("Rectangle") && tokens.length == 3){
+						try{
+							double value1 = Double.parseDouble(tokens[1]);
+							double value2 = Double.parseDouble(tokens[2]);
+							arrayOfShapes[index] = new Rectangle(value1, value2); // create Rectangle object
+							index++;
+						}catch(ParallelogramException e){ // use superclass custom exception handler
+							System.out.println(e.getMessage());
+						}
+					}else if(tokens[0].equals("Square") && tokens.length == 2){
+						try{
+							double value = Double.parseDouble(tokens[1]);
+							arrayOfShapes[index] = new Square(value); // create Square object
+							index++;
+						} catch(ParallelogramException e){ // use superclass custom exception handler
+							System.out.println(e.getMessage());
+						}
+					}else{
+						continue; //ignore and continue read a file
+					}
+			}
+				
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally { // must execute 
+			System.out.println(); // print blank line 
+			System.out.println(index + " shapes were created:");
+		
+			for(Shape shape : arrayOfShapes){ // use for each loop print Shape array elements
+				if(shape == null)
+					break;
+				System.out.println(shape);
+			}
+			
+		}
+	}
+}
---
+++ jac444_ws4_5/src/jac444_ws4_5/StudentObjectOutputStream.java
@@ -0,0 +1,74 @@
+package jac444_ws4_5;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class StudentObjectOutputStream {
+	private static ObjectOutputStream output;// output data to file
+	
+	public static void main(String[] args){
+		openFile();
+		addStudents();
+		closeFile();
+	}
+	// open file studnets.ser
+	public static void openFile(){
+		try{
+			//truncate and overwrite an existing file, or create the file if it doesn't initially exist
+			//Converts the given URI to a Path object
+			output = new ObjectOutputStream(Files.newOutputStream(Paths.get("students.ser")));
+		}catch(IOException err){
+			System.err.println("Error openin file.");
+			System.exit(1); //terminate the program
+		}
+	}
+	// add student informations to file
+	public static void addStudents(){
+		Scanner input = new Scanner(System.in);
+		System.out.printf("%s%n%s%nEnter: ", 
+				"Enter Student ID, First Name, Last Name, and Courses.",
+				"End input Ctrl+Z for Windows, Ctrl+D for Mac.");
+		
+		while(input.hasNext()){ // loop through until EOF
+			try{		
+				Student student = new Student(); // call default constructor
+			
+				student.setStID(input.nextInt());
+				student.setFirstName(input.next());
+				student.setLastName(input.next());
+				
+				//extract courses
+				String[] tokens = input.nextLine().split("\\s");
+				//set the courses
+				for(int i = 0; i < tokens.length; i++){
+					if(tokens[i].equals("")){ 
+						continue;
+					}else
+						student.setCourses(tokens[i]);
+				}
+				output.writeObject(student); //serialize student object into file
+			}catch (NoSuchElementException elementException){
+				System.err.println("Invalid input. Please try again.");
+				input.nextLine(); // discard input so user can try again
+			}catch (IOException err){
+				System.err.println("Error writing to file. Terminating.");
+				break;
+			} 
+			System.out.print("Enter: ");
+		}
+		input.close(); //prevent memory leak
+	}
+	// close students.ser file
+	public static void closeFile(){
+		try{
+			if(output != null)
+				output.close();
+		}catch(IOException err){
+			System.err.println("Error open file.");
+		}
+	}
+}
